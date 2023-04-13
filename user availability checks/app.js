2f3ab3d2550e9094a8346f9dd08182abc7281714
Nikita <85712440+kisonich@users.noreply.github.com>
2023-04-13 18:43:34 +0500

    Update app.js

--- user availability checks/app.js
+++ user availability checks/app.js
@@ -1,32 +1,39 @@
-const users = require('./src/windows');
-const path = require('path');
-const express = require('express');
+const usersTable = require("./src/windows");
+const path = require("path");
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Настройка middleware
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-// Главная страница
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Веб-приложение' });
-});
-
-// Обработка формы пользователей
-app.post('/users', (req, res) => {
-  const name = req.body.name;
-  users((data) => {
-    const value = data.includes(name) ? 0 : -1;
-    const message = `${name} ${(value === -1) ? 'не найден' : 'найден'}`;
-    res.render('users', { title: `Пользователь ${name}`, name, value, message });
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Веб-приложение",
   });
 });
 
-// Запуск сервера
-app.listen(port, () => {
+app.post("/users", async (req, res) => {
+  const name = req.body.name;
+  try {
+    const users = await usersTable();
+    const value = users.includes(name) ? 0 : -1;
+    const message = value === -1 ? `Пользователя ${name} не существует` : `Пользователь ${name} найден`;
+
+    res.render("users", {
+      title: message,
+      name,
+      value,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Ошибка сервера");
+  }
+});
+
+const server = app.listen(port, () => {
   console.log(`Приложение запущено на http://localhost:${port}`);
 });
